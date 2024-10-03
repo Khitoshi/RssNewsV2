f625ef194ed0b3ff00dc6885383b401e13aaf60a
kHitoshi <kubotah1115@gmail.com>
2024-10-04 01:22:26 +0900

    rssfeed表示処理作成 未リファクタリング

--- Back/main.go
+++ Back/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 	"time"
+
+	"github.com/mmcdole/gofeed"
 )
 
 type HelloHandler struct{}
@@ -26,6 +30,61 @@ func (h *HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(`{"message": "Hello, World!"}`))
 }
 
+type RssFeedHandler struct{}
+
+type RssFeedItem struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Link        string `json:"link"`
+}
+
+type Response struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+
+	FeedDataItems []RssFeedItem `json:"feedDataItems"`
+}
+
+func (h *RssFeedHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// Set CORS headers
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	// Handle preflight OPTIONS request
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	// Handle actual GET request
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	//w.Write([]byte(`{"message": "Hello, World!"}`))
+
+	feed, err := gofeed.NewParser().ParseURL("https://zenn.dev/spiegel/feed")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+
+	feedDataItems := []RssFeedItem{}
+
+	for _, item := range feed.Items {
+		feedDataItems = append(feedDataItems, RssFeedItem{Title: item.Title, Description: item.Description, Link: item.Link})
+	}
+
+	response := Response{Title: feed.Title, Description: feed.Description, FeedDataItems: feedDataItems}
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Printf("response: %v", jsonResponse)
+	//w.Write([]byte(` {"message": feed.Title}`))
+	w.Write(jsonResponse)
+}
+
 func main() {
 	//スタート時間・処理時間表示
 	startTime := time.Now()
@@ -39,7 +98,12 @@ func main() {
 		Handler: nil,
 	}
 	HelloHandler := HelloHandler{}
+
+	// APIのエンドポイントを設定
 	http.Handle("/hello", &HelloHandler)
+
+	RssFeedHandler := RssFeedHandler{}
+	http.Handle("/get_feed", &RssFeedHandler)
 
 	server.ListenAndServe()
 }
--- Front/rss-news-v2/src/App.tsx
+++ Front/rss-news-v2/src/App.tsx
@@ -1,22 +1,43 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
+interface feedDataItem {
+  title: string;
+  description: string;
+  link: string;
+}
+
+interface feedData {
+  title: string;
+  description: string;
+  feedDataItems: feedDataItem[];
+}
+
 function App() {
-  const [data, setData] = useState<string>('');
+  const [data, setData] = useState<feedData>();
 
   useEffect(() => {
-    fetch('http://localhost:8080/hello') // GolangのAPIエンドポイント
+    fetch('http://localhost:8080/get_feed') // GolangのAPIエンドポイント
       .then(response => response.json())
-      .then(d => setData(d.message))
+      .then((d: feedData) => setData(d))
       .catch(error => console.error('Error:', error));
   }, []);
 
   return (
     <div>
-      <h1>{data || 'Loading...'}</h1>
+      <h1>{data?.title || 'Loading...'}</h1>
+      <h1>{data?.description || 'Loading...'}</h1>
+      <div>
+        {(data?.feedDataItems ?? []).map((item, index) => (
+          <div key={index}>
+            <h2>{index}: {item.title}</h2>
+            <p>{item.description}</p>
+            <a href={item.link}>{item.link}</a>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
